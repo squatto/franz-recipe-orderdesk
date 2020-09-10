92782ecfa767fecd1bebe2df3114fbc676476e05
Scott Carpenter <scott@payforstay.com>
2020-09-10 00:26:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Order Desk for Franz/Ferdi
+
+This is an unofficial Franz/Ferdi recipe for [Order Desk](https://www.orderdesk.com/).
+
+The badge on the service icon will show the number of open orders.
+Change the value of the `folderId` variable in `webview.js` to the ID of the folder
+in Order Desk that you want to show the open order count for.
+
+e.g. `https://app.orderdesk.me/orders?folder=123456` = `folderId 123456`
+
+[GitHub repo](https://github.com/squatto/franz-recipe-orderdesk)
+
+## Installation
+
+Follow the instructions [here](https://blog.jakelee.co.uk/super-simple-guide-to-adding-a-new-ferdi-service-recipe/)
+and put the files in this repo in `/recipes/dev/orderdesk/`
+
+### How to create your own Franz recipes
+
+* [Read the documentation](https://github.com/meetfranz/plugins)
---
+++ webview.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = Franz => {
+  const contains = function (selector, text) {
+    const elements = document.querySelectorAll(selector);
+    return Array.prototype.filter.call(elements, function(element){
+      return RegExp(text).test(element.textContent);
+    });
+  }
+
+  const getMessages = function getMessages() {
+    let unread = 0;
+
+    // change this to the folderId that you want to
+    // show the order count for on the service icon badge
+    // e.g. https://app.orderdesk.me/orders?folder=123456 = 123456
+    const folderId = 123456;
+
+    const countObject = document.getElementById(`folder_count_${folderId}`);
+
+    if (countObject !== undefined) {
+      let countText = countObject.innerText.replace(',', '');
+
+      if (countText.substr(-2).toUpperCase() === 'K+') {
+        countText = countText.substr(0, countText.length - 2) + '000';
+      }
+
+      unread = parseInt(countText, 10);
+    }
+
+    Franz.setBadge(parseInt(unread, 10));
+  };
+
+  Franz.loop(getMessages);
+};
